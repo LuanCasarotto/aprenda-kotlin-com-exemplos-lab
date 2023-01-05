5381fed6f3ad2ee89b6fb76289bbc4f4fa14741a
Luan Menegatti Casarotto <43160675+LuanCasarotto@users.noreply.github.com>
2023-01-05 18:33:58 -0300

    Refatorando código para a Main ficar mais legível

--- desafio.kt
+++ desafio.kt
@@ -23,11 +23,38 @@ fun main() {
     // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 
     val luan: Usuario = Usuario("Luan")
-    val listaConteudos = mutableListOf<ConteudoEducacional>(
-    ConteudoEducacional("kotlin Experience"),
-    ConteudoEducacional("Android 1", 30),
-    ConteudoEducacional("Android 2", 30)
+    val lucas: Usuario = Usuario("Lucas")
+    val wellington: Usuario = Usuario("Wellington")
+
+    val listaConteudosAndroid = getListaConteudo("Android")
+    println(listaConteudosAndroid)
+    val listaConteudosBackEnd = getListaConteudo("Backend")
+    val listaConteudosFrontEnd = getListaConteudo("Frontend")
+
+    val formacao: Formacao = Formacao("Android", listaConteudosAndroid) 
+    println(formacao)
+}
+
+fun getListaConteudo(seguimento: String): mutableListOf<ConteudoEducacional> {
+    when(seguimento) {
+        "Android" -> mutableListOf<ConteudoEducacional>(
+    ConteudoEducacional("Fundamentos em Kotlin", 200),
+    ConteudoEducacional("Android 1"),
+    ConteudoEducacional("Android 2", 35),
+    ConteudoEducacional("Android 3", 50)
     )
-    val formacaco: Formacao = Formacao("Android", listaConteudos) 
-    println(formacaco)
+        "Backend" -> mutableListOf<ConteudoEducacional>(
+    ConteudoEducacional("Fundamentos em Java", 240),
+    ConteudoEducacional("Backend 1"),
+    ConteudoEducacional("Backend 2", 55),
+    ConteudoEducacional("Backend 3", 50)
+    )
+        "Frontend" -> mutableListOf<ConteudoEducacional>(
+    ConteudoEducacional("Fundamentos em JavaScript", 180),
+    ConteudoEducacional("Frontend 1"),
+    ConteudoEducacional("Frontend 2", 25),
+    ConteudoEducacional("Frontend 3", 40)
+    )
+        else -> println("Formação não definida")
+    }
 }
