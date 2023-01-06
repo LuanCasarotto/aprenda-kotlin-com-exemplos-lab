34f8be8a38dd497aaf2fa0efb007641174c2b7e5
Luan Menegatti Casarotto <43160675+LuanCasarotto@users.noreply.github.com>
2023-01-05 23:07:12 -0300

    Implementação com níveis e usuários de exemplo

--- desafio.kt
+++ desafio.kt
@@ -2,18 +2,16 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-data class Usuario (val nome: String)
+data class Usuario(val nome: String)
 
 data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
 
     fun matricular(usuario: Usuario) {
         inscritos.add(usuario)
-        println(inscritos)
-
         // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
@@ -26,44 +24,121 @@ fun main() {
     val lucas: Usuario = Usuario("Lucas")
     val wellington: Usuario = Usuario("Wellington")
 
-    val listaConteudosAndroid = getListaConteudo("Android")
-    //println(listaConteudosAndroid)
-    val listaConteudosBackEnd = getListaConteudo("Backend")
-    val listaConteudosFrontEnd = getListaConteudo("Frontend")
+    val listaConteudosAndroid: List<ConteudoEducacional> = getListaConteudoBasico("Android")
+    val listaConteudosFrontEnd: List<ConteudoEducacional> = getListaConteudoIntermediario("Frontend")
+    val listaConteudosBackEnd: List<ConteudoEducacional> = getListaConteudoAvançado("Backend")
 
-    val formacao: Formacao = Formacao("Android", listaConteudosAndroid) 
-    println(formacao)
+    val formacaoAndroid: Formacao = Formacao("Android", listaConteudosAndroid, Nivel.BASICO)
+    val formacaoFrontEnd: Formacao = Formacao("Frontend", listaConteudosFrontEnd, Nivel.INTERMEDIARIO)
+    val formacaoBackEnd: Formacao = Formacao("Backend", listaConteudosBackEnd, Nivel.AVANÇADO)
+
+    formacaoAndroid.matricular(luan)
+    formacaoAndroid.matricular(wellington)
+    formacaoAndroid.matricular(lucas)
+
+    formacaoFrontEnd.matricular(lucas)
+
+    formacaoBackEnd.matricular(luan)
+    formacaoBackEnd.matricular(wellington)
+
+    println("$formacaoAndroid \n Inscritos: ${formacaoAndroid.inscritos}")
+    println("$formacaoFrontEnd \n Inscritos: ${formacaoFrontEnd.inscritos}")
+    println("$formacaoBackEnd \n Inscritos: ${formacaoBackEnd.inscritos}")
 }
 
-fun getListaConteudo(seguimento: String): List<ConteudoEducacional> {
-    when(seguimento) {
+fun getListaConteudoBasico(seguimento: String): List<ConteudoEducacional> {
+    when (seguimento) {
         "Android" -> {
-             return mutableListOf<ConteudoEducacional>(
-             ConteudoEducacional("Fundamentos em Kotlin", 200),
-             ConteudoEducacional("Android 1"),
-             ConteudoEducacional("Android 2", 35),
-             ConteudoEducacional("Android 3", 50)
-    )}
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Kotlin e seus fundamentos"),
+                ConteudoEducacional("Android Básico")
+            )
+        }
+
         "Backend" -> {
             return mutableListOf<ConteudoEducacional>(
-            ConteudoEducacional("Fundamentos em Java", 240),
-            ConteudoEducacional("Backend 1"),
-            ConteudoEducacional("Backend 2", 55),
-            ConteudoEducacional("Backend 3", 50)
-    )}
+                ConteudoEducacional("Java e seus fundamentos", 240),
+                ConteudoEducacional("Backend Básico", 65)
+            )
+        }
+
         "Frontend" -> {
             return mutableListOf<ConteudoEducacional>(
-            ConteudoEducacional("Fundamentos em JavaScript", 180),
-            ConteudoEducacional("Frontend 1"),
-            ConteudoEducacional("Frontend 2", 25),
-            ConteudoEducacional("Frontend 3", 40)
-    )}
+                ConteudoEducacional("JavaScript e seus fundamentos", 180),
+                ConteudoEducacional("Frontend Básico")
+            )
+        }
+
         else -> {
             return mutableListOf<ConteudoEducacional>(
-            ConteudoEducacional("Curso padrão"),
-            ConteudoEducacional("Padrão 1"),
-            ConteudoEducacional("Padrão 2"),
-            ConteudoEducacional("Padrão 3")
-        )}
+                ConteudoEducacional("Curso padrão e fundamentos"),
+                ConteudoEducacional("Lógica Básico", 90)
+            )
+        }
+    }
+}
+
+fun getListaConteudoIntermediario(seguimento: String): List<ConteudoEducacional> {
+    when (seguimento) {
+        "Android" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Kotlin Intermediário", 200),
+                ConteudoEducacional("Android Intermediário", 35)
+            )
+        }
+
+        "Backend" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Java Intermediário", 240),
+                ConteudoEducacional("Backend Intermediário", 55)
+            )
+        }
+
+        "Frontend" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("JavaScript Intermediário", 180),
+                ConteudoEducacional("Frontend Intermediário", 25)
+
+            )
+        }
+
+        else -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Curso padrão Intermediário", 130),
+                ConteudoEducacional("Lógica Intermediário", 90)
+            )
+        }
+    }
+}
+
+fun getListaConteudoAvançado(seguimento: String): List<ConteudoEducacional> {
+    when (seguimento) {
+        "Android" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Kotlin Avançado", 45),
+                ConteudoEducacional("Android Avançado", 50)
+            )
+        }
+
+        "Backend" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Java Avançado", 240),
+                ConteudoEducacional("Backend Avançado", 50)
+            )
+        }
+
+        "Frontend" -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("JavaScript Avançado", 180),
+                ConteudoEducacional("Frontend Avançado", 40)
+            )
+        }
+
+        else -> {
+            return mutableListOf<ConteudoEducacional>(
+                ConteudoEducacional("Curso padrão Avançado", 90),
+                ConteudoEducacional("Lógica Avançado", 90)
+            )
+        }
     }
 }
